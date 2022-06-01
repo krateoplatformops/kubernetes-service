9d611dc7be8d44acb90ca7c8cd917aeae134bdfa
mauro sala <mauro.sala@gmail.com>
2022-06-01 15:23:09 +0200

    feat(read): remove package

--- constants/index.js
+++ constants/index.js
@@ -1,9 +1,7 @@
 const envConstants = require('./env.constants')
 const pathConstants = require('./path.constants')
-const packageConstants = require('./package.constants')
 
 module.exports = {
   envConstants,
   pathConstants,
-  packageConstants
 }
